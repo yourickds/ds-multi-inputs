3385adb0359ab1b70ed319fae9621aef58647810
yourick <youricks1@gmail.com>
2023-09-12 21:10:55 +0700

    Добавил возможность указывать минимальное и максимальное кол-во элементов, а так-же переписал на класс

--- ds-multi-inputs.js
+++ ds-multi-inputs.js
@@ -1,60 +1,82 @@
-function createDsMultiInputs(el, params)
-{ 
-    console.log(params);
-    let ds_multi_inputs = document.createElement('div');
-    ds_multi_inputs.classList.add("ds-multi-inputs");
-
-    let items = document.createElement('div');
-    items.classList.add("ds-multi-inputs__items");
-
-    items.append(createItem(el));
-
-    ds_multi_inputs.append(items);
-
-    el.after(ds_multi_inputs);
-    el.remove();
-}
-
-function createItem(el)
+class CreateDsMultiInputs
 {
-    let item = document.createElement('div');
-    item.classList.add("ds-multi-inputs__items_item");
+    #min;
+    #max;
+    #elements = 0;
 
-    let input = document.createElement('div');
-    input.classList.add("ds-multi-inputs__items_item__input");
-
-    let actions = document.createElement('div');
-    actions.classList.add("ds-multi-inputs__items_item__actions");
-
-    let minus = document.createElement('button');
-    minus.innerText = "minus";
-    minus.onclick = function(event){
-        event.preventDefault();
-        item.remove();
+    constructor(min = 1, max = null) {
+        this.#min = min;
+        this.#max = max;
     }
 
-    let plus = document.createElement('button');
-    plus.innerText = "plus";
-    plus.onclick = function(event) {
-        event.preventDefault();
-        item.after(createItem(el));
+    createDsMultiInputs(el, params) { 
+        console.log(params);
+        let ds_multi_inputs = document.createElement('div');
+        ds_multi_inputs.classList.add("ds-multi-inputs");
+
+        let items = document.createElement('div');
+        items.classList.add("ds-multi-inputs__items");
+        for (let i = 0; i < this.#min; i++) {
+            items.append(this.createItem(el));
+        }
+
+        ds_multi_inputs.append(items);
+
+        el.after(ds_multi_inputs);
+        el.remove();
     }
 
-    actions.append(minus);
-    actions.append(plus);
+    createItem(el) {
+        let item = document.createElement('div');
+        item.classList.add("ds-multi-inputs__items_item");
 
-    input.append(el.cloneNode(true));
+        let input = document.createElement('div');
+        input.classList.add("ds-multi-inputs__items_item__input");
 
-    item.append(input);
-    item.append(actions);
+        let actions = document.createElement('div');
+        actions.classList.add("ds-multi-inputs__items_item__actions");
 
-    return item;
+        let minus = document.createElement('button');
+        minus.innerText = "-";
+        minus.onclick = (event) => {
+            event.preventDefault();
+            if (this.#elements > this.#min) {
+                item.remove();
+                this.#elements--;
+            }
+        }
+
+        let plus = document.createElement('button');
+        plus.innerText = "+";
+        plus.onclick = (event) => {
+            event.preventDefault();
+            if (this.#elements < this.#max)
+                item.after(this.createItem(el));
+        }
+
+        actions.append(minus);
+        actions.append(plus);
+
+        input.append(el.cloneNode(true));
+
+        item.append(input);
+        item.append(actions);
+
+        this.#elements++;
+
+        console.log(this.#elements);
+
+        return item;
+    }
 }
 
-Element.prototype.ds_multi_inputs = function(params) {
-    createDsMultiInputs(this, params);
-}
+// Element.prototype.ds_multi_inputs = function(params) {
+//     new CreateDsMultiInputs.createDsMultiInputs(this, params);
+// }
 
 NodeList.prototype.ds_multi_inputs = function(params) {
-    this.forEach((el) => createDsMultiInputs(el, params))
+    this.forEach((el) => {
+        let create = new CreateDsMultiInputs(params.min, params.max);
+        create.createDsMultiInputs(el, params);
+    })
 }
--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 document.addEventListener('DOMContentLoaded', function(){
-    document.querySelectorAll('[data-action="ds-multi-inputs"]')
+    document.querySelectorAll('#ffff')
         .ds_multi_inputs({
-            min: 2
+            min: 1,
+            max: 5
+        });
+
+    document.querySelectorAll('#ss')
+        .ds_multi_inputs({
+            min: 2,
+            max: 3
+        });
+
+    document.querySelectorAll('#pppp')
+        .ds_multi_inputs({
+            min: 5,
+            max: 8
         });
 });
